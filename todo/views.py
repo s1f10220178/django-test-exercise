b77281c68741764a6b8eb18a16cacb758143b5e4
1F10220178 <s1F102201784@iniad.org>
2023-07-06 14:06:46 +0900

    警告を修正①

--- todo/views.py
+++ todo/views.py
@@ -3,7 +3,7 @@ from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
 from todo.models import Task
 
-# Create your views here.
+
 def index(request):
     if request.method == 'POST':
         task = Task(title=request.POST['title'],
@@ -17,6 +17,6 @@ def index(request):
 
 
     context = {
-        'tasks': tasks
+         'tasks': tasks
     }
     return render(request, 'todo/index.html', context)
